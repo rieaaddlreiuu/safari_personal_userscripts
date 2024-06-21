ff5ccd5a604dbaa0e26865c9b105f595947db254
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2024-06-22 00:17:45 +0900

    おためし

---
+++ test.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         test-script
+// @namespace    http://shukapin.com/
+// @version      0.1
+// @description  Simple UserScript for test
+// @author       Shukapin
+// @match        https://shukapin.com/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+alert("UserScriptが起動しました！");
+GM_addStyle("body { background-color: hotpink;}");
