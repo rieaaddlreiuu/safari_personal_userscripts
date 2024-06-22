c0ef85fc506067eb5776f4926293f5003dcaf432
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2024-06-22 11:55:38 +0900

    sde

--- test.js
+++ test.js
@@ -4,7 +4,8 @@
 // @version 0.1
 // @description try to take over the world!
 // @author You
-// @match https://nicovideo.jp/*
+// @match https://www.nicovideo.jp/*
+// @downloadURL https://raw.githubusercontent.com/rieaaddlreiuu/safari_personal_userscripts/main/test.js
 // ==/UserScript==
 
 (function() {
