0695de8e9a42a210596b109588c4a9c3d6182bff
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2024-06-22 12:38:50 +0900

    ks

--- test.user.js
+++ test.user.js
@@ -10,7 +10,9 @@
 // ==/UserScript==
 
 let elems = document.getElementsByClassName("d_flex flex-wrap_wrap gap_8px")
-alert(elems);
+elems.forEach(element => {
+    alert(element);    
+});
 /*
 <div class="d_flex flex-wrap_wrap gap_8px">
     <span class="d_inline-flex items_center p_3px_12px bg_#fff rounded_10000px fs_12px border_#d0d0d0_solid_1px white-space_nowrap">アニメ</span>
