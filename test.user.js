051ba17a9fff41f687dbf9d6fb50796f4724f633
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2024-06-22 12:36:23 +0900

    ちょっとした変更

--- test.user.js
+++ test.user.js
@@ -9,4 +9,17 @@
 // @updateURL https://raw.githubusercontent.com/rieaaddlreiuu/safari_personal_userscripts/main/test.user.js
 // ==/UserScript==
 
-alert("nafco");
+let elems = document.getElementsByClassName("d_flex flex-wrap_wrap gap_8px")
+alert("elems");
+/*
+<div class="d_flex flex-wrap_wrap gap_8px">
+    <span class="d_inline-flex items_center p_3px_12px bg_#fff rounded_10000px fs_12px border_#d0d0d0_solid_1px white-space_nowrap">アニメ</span>
+    <span class="d_inline-flex items_center p_3px_12px bg_#fff rounded_10000px fs_12px border_#d0d0d0_solid_1px white-space_nowrap">とある科学の超電磁砲</span>
+    <span class="d_inline-flex items_center p_3px_12px bg_#fff rounded_10000px fs_12px border_#d0d0d0_solid_1px white-space_nowrap">ファンタCMシリーズ</span>
+    <span class="d_inline-flex items_center p_3px_12px bg_#fff rounded_10000px fs_12px border_#d0d0d0_solid_1px white-space_nowrap">MAD</span>
+    <span class="d_inline-flex items_center p_3px_12px bg_#fff rounded_10000px fs_12px border_#d0d0d0_solid_1px white-space_nowrap">ファンタスティック</span>
+    <span class="d_inline-flex items_center p_3px_12px bg_#fff rounded_10000px fs_12px border_#d0d0d0_solid_1px white-space_nowrap">変態画質</span>
+    <span class="d_inline-flex items_center p_3px_12px bg_#fff rounded_10000px fs_12px border_#d0d0d0_solid_1px white-space_nowrap">熱帯果実(トロピカルフルーツ)</span>
+    <span class="d_inline-flex items_center p_3px_12px bg_#fff rounded_10000px fs_12px border_#d0d0d0_solid_1px white-space_nowrap">白長井</span>
+</div>
+*/
