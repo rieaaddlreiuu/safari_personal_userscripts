9ab9d1a577261bb232aba2af82778848126b8a48
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2024-06-23 18:12:42 +0900

    ようつべNG機能を開発するよ

---
+++ youtube_ng.user.js
@@ -0,0 +1,19 @@
+// @name ようつべコメントNG機能です
+// @namespace http://tampermonkey.net/
+// @version 0.1
+// @description try to take over the world!
+// @author You
+// @match https://www.youtube.com/watch*
+// @downloadURL https://raw.githubusercontent.com/rieaaddlreiuu/safari_personal_userscripts/main/test.user.js
+// @updateURL https://raw.githubusercontent.com/rieaaddlreiuu/safari_personal_userscripts/main/test.user.js
+// ==/UserScript==
+
+(function () {
+  })();
+  /*
+  <div class="d_flex flex-wrap_wrap gap_8px">
+  <span class="d_inline-flex items_center p_3px_12px bg_#fff rounded_10000px fs_12px border_#d0d0d0_solid_1px white-space_nowrap">アニメ</span>
+  ...
+  </div>
+  */
+  
