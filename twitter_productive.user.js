7dcfbf68350f19bf788b4eab284be335d2dcfbdb
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2024-07-23 00:11:46 +0900

    クイズを出題するようにした

--- twitter_productive.user.js
+++ twitter_productive.user.js
@@ -50,19 +50,26 @@
             location.href = url;
         }
     })
+    function random(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+    let quiz_position = 100;
     cyclicExecute(1000, () => {
-        tweetsForEach((tweet) => {
-            //if (tweet.getAttribute("id") != "processed") {
+        /*tweetsForEach((tweet) => {
+            if (tweet.getAttribute("id") != "processed") {
                 tweet.setAttribute("id", "processed");
                 //let tweet_text = tweet.getElementsByClassName("css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-poiln3");
-                let text = tweet.innerText.split("\n");
+                /*let text = tweet.innerText.split("\n");
                 if (text[2] == "Ad") {
                     let content = tweet.getElementsByTagName("article")[0];
                     let quiz = `물리학 입문 <b>과제</b>가 너무 많다!太字部分の意味は？<br>
                     (1) 価値<br>
                     (2) 課題<br>
                     (3) 内容<br>
-                    <details>`;
+                    <details>
+                    <summary>答え</summary>
+                    (2)の「課題」！
+                    </details>`;
                     let text = content.children[0].children[0].children[1].children[1].children[1];
                     let pictures = content.children[0].children[0].children[1].children[1].children[2];
                     pictures.outerHTML = "";
@@ -70,9 +77,31 @@
                    //文字 : first.first.last.last.(2番目) 0 0 1 1 1
                    //画像 : first.first.last.last.(3番目) 0 0 1 1 2
                 }
+               //tweet.style = "";
+               console.log(tweet.innerText);
                 //tweet.parentNode.insertAdjacentHTML("beforebegin",'<div class="css-175oi2r r-j5o65s r-qklmqi r-1adg3ll r-1ny4l3l">'+tweet.innerHTML+'</div>');
-            //}
-        })
+            }
+        })*/
+        quiz_position += random(200, 1000);
+        let timeline_rect = document.querySelector('[role="main"]').getBoundingClientRect();
+        if (quiz_position < timeline_rect.height) {
+            let left_margin = document.querySelector('[role="banner"]').getBoundingClientRect().width;
+            let test_html = `<div style="position:absolute; top: ` + quiz_position + `px; left: ` + left_margin + `px; z-index: 10000; background-color: #FFFFFF; width: ` + timeline_rect.width + `px;">물리학 입문 <b>과제</b>가 너무 많다!太字部分の意味は？<br>
+            <div>(1) 価値</div>
+            <div class="correct_answer">(2) 課題</div>
+            <div>(3) 内容</div>
+                    <details>
+                    <summary>答え</summary>
+                    (2)の「課題」！
+                    </details></div>`;
+            document.getElementById("react-root").children[0].children[0].children[0].insertAdjacentHTML("afterend", test_html);
+            let correct_answer_elements = document.getElementsByClassName("correct_answer");
+            for(let i=0;i<correct_answer_elements.length;i++){
+                correct_answer_elements[i].addEventListener("click", function (){
+                    this.parentNode.style = "display:none;";
+                });
+            }
+        }
     })
 })();
 
