005c45196f0098957a9316380a33e29b7af2778c
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2024-07-26 19:36:01 +0900

    密度調整1

--- twitter_productive.user.js
+++ twitter_productive.user.js
@@ -58,7 +58,7 @@
     function random(min, max) {
         return Math.random() * (max - min) + min;
     }
-    let quiz_position = random(200, 1000);
+    let quiz_position = 1000;
     let style = document.createElement('style');
     style.innerHTML = `
         .button019 a {
@@ -111,33 +111,6 @@
 `;
     document.head.appendChild(style);
     cyclicExecute(100, () => {
-        /*tweetsForEach((tweet) => {
-            if (tweet.getAttribute("id") != "processed") {
-                tweet.setAttribute("id", "processed");
-                //let tweet_text = tweet.getElementsByClassName("css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-poiln3");
-                /*let text = tweet.innerText.split("\n");
-                if (text[2] == "Ad") {
-                    let content = tweet.getElementsByTagName("article")[0];
-                    let quiz = `물리학 입문 <b>과제</b>가 너무 많다!太字部分の意味は？<br>
-                    (1) 価値<br>
-                    (2) 課題<br>
-                    (3) 内容<br>
-                    <details>
-                    <summary>答え</summary>
-                    (2)の「課題」！
-                    </details>`;
-                    let text = content.children[0].children[0].children[1].children[1].children[1];
-                    let pictures = content.children[0].children[0].children[1].children[1].children[2];
-                    pictures.outerHTML = "";
-                    text.innerHTML = quiz;
-                   //文字 : first.first.last.last.(2番目) 0 0 1 1 1
-                   //画像 : first.first.last.last.(3番目) 0 0 1 1 2
-                }
-               //tweet.style = "";
-               console.log(tweet.innerText);
-                //tweet.parentNode.insertAdjacentHTML("beforebegin",'<div class="css-175oi2r r-j5o65s r-qklmqi r-1adg3ll r-1ny4l3l">'+tweet.innerHTML+'</div>');
-            }
-        })*/
         let timeline_rect = document.querySelector('[role="main"]').getBoundingClientRect();
         console.log(timeline_rect.height);
         if (quiz_position < timeline_rect.height) {
@@ -168,7 +141,7 @@
                     this.parentNode.style = "display:none;";
                 });
             }
-            quiz_position += random(200, 1000);
+            quiz_position += random(600, 3000);
         }
     })
 })();
