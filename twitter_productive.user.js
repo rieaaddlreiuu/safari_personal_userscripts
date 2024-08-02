2bd2611e3ae1ad35fdafdbfbf3069eb4cc33a75f
rieaaddlreiuu <133306044+rieaaddlreiuu@users.noreply.github.com>
2024-08-03 02:58:53 +0900

    Update twitter_productive.user.js

--- twitter_productive.user.js
+++ twitter_productive.user.js
@@ -6,7 +6,7 @@
 // @author You
 // @require https://raw.githubusercontent.com/rieaaddlreiuu/safari_personal_userscripts/develop/twitter_productive/modules/twipro_quiz_modules.js?token=GHSAT0AAAAAACTHY7EZNPGBQSEBAT5QGGFKZVFFMNQ
 // @require https://raw.githubusercontent.com/rieaaddlreiuu/safari_personal_userscripts/develop/twitter_productive/modules/basic_features.js?token=GHSAT0AAAAAACTHY7EZZROJFYCXN4HX3N6KZVFFLNQ
-// @require https://192.168.2.102/index/products/output/twipro_output.js
+// @require https://raw.githubusercontent.com/rieaaddlreiuu/rieaaddlreiuu/main/files/twipro_quiz_data.js
 // @match https://twitter.com/*
 // @match https://x.com/*
 // ==/UserScript==
