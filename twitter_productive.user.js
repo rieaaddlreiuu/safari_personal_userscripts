f5152f0e0c6ffe00104d1c06e0e34060755611f0
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2024-08-06 00:18:52 +0900

    大胆すぎるぜ

--- twitter_productive.user.js
+++ twitter_productive.user.js
@@ -8,10 +8,6 @@
 // @grant        GM_getResourceText
 // @match https://twitter.com/*
 // @match https://x.com/*
-// @resource Twipro_css https://raw.githubusercontent.com/rieaaddlreiuu/safari_personal_userscripts/develop/twitter_productive/modules/style.css
-// @require https://raw.githubusercontent.com/rieaaddlreiuu/safari_personal_userscripts/develop/twitter_productive/modules/twipro_quiz_modules.js
-// @require https://raw.githubusercontent.com/rieaaddlreiuu/safari_personal_userscripts/develop/twitter_productive/modules/basic_features.js
-// @require https://raw.githubusercontent.com/rieaaddlreiuu/rieaaddlreiuu/main/files/twipro_quiz_data.js
 // ==/UserScript==
 
 (function () {
@@ -21,7 +17,58 @@
         }
         return true;
     }
-    GM_addStyle(GM_getResourceText("Twipro_css"))
+    let style = document.createElement('style');
+    style.innerHTML = `
+        .button019 a {
+    background: #eee;
+    border-radius: 3px;
+    position: relative;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    max-width: 280px;
+    padding: 10px 25px;
+    color: #313131;
+    transition: 0.3s ease-in-out;
+    font-weight: 500;
+}
+.button019 a:after {
+  content: "";
+  position: absolute;
+  top: 50%;
+  bottom: 0;
+  right: 2rem;
+  font-size: 90%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: right 0.3s;
+  width: 6px;
+  height: 6px;
+  border-top: solid 2px currentColor;
+  border-right: solid 2px currentColor;
+  transform: translateY(-50%) rotate(45deg);
+}
+.button019 a:hover {
+  background: #6bb6ff;
+  color: #FFF;
+}
+.button019 a:hover:after {
+  right: 1.4rem;
+}
+.box1 {
+    padding: 0.5em 1em;
+    margin: 2em 0;
+    font-weight: bold;
+    border: solid 3px #000000;
+}
+.box1 p {
+    margin: 0; 
+    padding: 0;
+}
+`;
+    style.innerHTML = '';
+    document.head.appendChild(style);
     cyclicExecute(1000, () => {
         if (!is_twitter(location.href)) {
             let url_head = location.href.substring(0, 8);
