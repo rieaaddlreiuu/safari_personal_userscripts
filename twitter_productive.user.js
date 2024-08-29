51b9f56b63cb6b0dc39f46ebe5a687e62cf5e668
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2024-08-30 00:34:12 +0900

    ばかさき

--- twitter_productive.user.js
+++ twitter_productive.user.js
@@ -121,7 +121,7 @@
             quiz_element.getElementsByClassName("show_answer")[0].addEventListener('click', function () {
                 this.children[1].style = "";
             });
-            set_quiz_time += 20 * f((CA_count) / (WA_count + CA_count));
+            set_quiz_time += f((CA_count) / (WA_count + CA_count));
         }
         if (document.getElementById("time_display") != null) {
             document.getElementById("time_display").innerHTML = `
