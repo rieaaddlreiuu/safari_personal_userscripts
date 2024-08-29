1ba32f77fcf76b570b7b82f9ab1446a0a349c233
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2024-08-30 00:13:37 +0900

    ttt

--- twitter_productive.user.js
+++ twitter_productive.user.js
@@ -126,7 +126,6 @@
                 right: 30px;
                 z-index: 1000;
             "></div>`);
-            //にゃーん
         }
         console.log(set_quiz_time);
         set_quiz_time--;
